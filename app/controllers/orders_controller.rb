91c033c45d3c9d44cb1f7f1e8bb482faa6558011
EC2 Default User <ec2-user@ip-172-31-21-125.us-west-2.compute.internal>
2019-08-14 02:25:28 +0000

    added a few final comments to the order controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,23 +7,29 @@ class OrdersController < ApplicationController
         @email = params[:email]
         @id = params[:itemId]
         
+        #get a customer by passing in their email
+        #returns an error if it's not found
         @code, @customer = Customer.getByEmail(@email)
         if @code == 404
            render json: {error: "Email not found for customer"}, status: 400
            return
         end
         
+        #see above, same thing, but for getting an item
         @code, @item = Item.getById(@id)
         if @code == 404
             render json: {error: "Item ID not found"}, status: 400
             return
         end
         
+        #logic for disallowing an order if the item isn't in stock
         if @item[:stockQty] <= 0
             render json: {error: "Item is not available to order"}, status: 400
             return
         end
         
+        #initializing a new order item as a hybrid of values
+        #from customer and item.
         @order.itemId = params[:itemId]
         @order.description = item[:description]
         @order.customerID = customer[:id]
